fd952bc1a37cb17cc1d306bc11ad85ce9b35b484
Shreyansh Shrey <shreyansh.shrey@velotio.com>
2023-05-18 18:03:58 +0530

    Removed transform

--- src/modules/bull-test/dtos/test-request.dto.ts
+++ src/modules/bull-test/dtos/test-request.dto.ts
@@ -1,26 +1,21 @@
 import { IsEnum, IsBoolean, IsOptional } from 'class-validator';
-import { Transform } from 'class-transformer';
 
 import { TestParameter } from '../../shared/enums/test-parameter.enum';
 
 export class TestRequestDto {
   @IsBoolean()
   @IsOptional()
-  @Transform(({ value }) => value ?? false)
   useMultipleQueue: boolean;
 
   @IsBoolean()
   @IsOptional()
-  @Transform(({ value }) => value ?? false)
   isJobGlobalScoped: boolean;
 
   @IsBoolean()
   @IsOptional()
-  @Transform(({ value }) => value ?? false)
   isJobComputationIntensive: boolean;
 
   @IsEnum(TestParameter)
   @IsOptional()
-  @Transform(({ value }) => value || TestParameter.BASIC)
   readonly testParameter: string;
 }
