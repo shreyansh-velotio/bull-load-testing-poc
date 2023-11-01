36a8f0ad8228543b63c9be5751f4931a1ce12f01
Shreyansh Shrey <105908147+shreyansh-velotio@users.noreply.github.com>
2023-11-01 17:27:44 +0530

    Update tasks.ts

--- src/modules/queues/consumer/tasks.ts
+++ src/modules/queues/consumer/tasks.ts
@@ -1,19 +1,19 @@
+// Perform a computationally intensive operation
 export function simulateComputationIntensiveTask() {
   const startTime = Date.now();
 
   let result = 0;
   while (Date.now() - startTime < 2000) {
-    // Perform a computationally intensive operation
     result += 1;
   }
 }
 
+// Perform a computationally light operation
 export function simulateComputationLightTask() {
   const startTime = Date.now();
 
   let result = 0;
   while (Date.now() - startTime < 500) {
-    // Perform a computationally light operation
     result += 1;
   }
 }
